45a411cf4275d271594634842565f9427dfbeff9
salvedojuliao <batistajulio215@gmail.com>
2022-08-17 23:03:43 -0300

    feat: tawild added

---
+++ intermediate/url-shortening-api-master/tailwind.config.js
@@ -0,0 +1,48 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        //Primary
+        p_cyan: 'hsl(180, 66%, 49%)',
+        p_dark_violet: 'hsl(257, 27%, 26%)',
+
+        //Secondary
+        p_red: 'hsl(0, 87%, 67%)',
+
+        // Neutral
+        p_gray: 'hsl(0, 0%, 75%)',
+        p_grayish_violet: 'hsl(257, 7%, 63%)',
+        p_very_dark_blue: 'hsl(255, 11%, 22%)',
+        p_very_dark_violet: 'hsl(260, 8%, 14%)',
+
+      }
+    },
+
+    screens: {
+      'sm': '375px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '640px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1440px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+
+    fontFamily:{
+      'poppins': ['Poppins', 'sans-serif'],
+    }
+    
+   
+ 
+  },
+  plugins: [],
+}
