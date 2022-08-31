bcc7ea519dea3b9fa86a15c53e50a4e721998a75
salvedojuliao <batistajulio215@gmail.com>
2022-08-31 07:48:53 -0300

    feat: add effects (translateY and Rotate) to each js_bar element of hamburguer

--- intermediate/url-shortening-api-master/src/assets/js/script.js
+++ intermediate/url-shortening-api-master/src/assets/js/script.js
@@ -1,6 +1,11 @@
 const btnMobile = document.getElementById('btn-mobile');
+const js_bar = document.querySelectorAll('.js_bar');
 
 btnMobile.addEventListener("click", ()=>{
     const nav = document.getElementById('nav-mobile');
     nav.classList.toggle('active')
+    js_bar.forEach(e => {
+        e.classList.toggle('active')
+    });
 })
+
